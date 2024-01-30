3faaacf9b465a62a2ba67c1b75e939b1f11081d4
mohammad yousefi <mohammad.yousefi.1996@gmail.com>
2024-01-30 23:29:42 +1100

    Added support for variables

--- src/lexical_analyzer/tests.rs
+++ src/lexical_analyzer/tests.rs
@@ -28,4 +28,47 @@ mod lexer_test {
             _ => panic!("wrong token")
         }
     }
+
+    #[test]
+    pub fn operation_recognition_test() {
+        let program = String::from("<=  \n> >= <").into_bytes();
+        let lexer = LexicalAnalyzer::new(program);
+        match lexer.get_token().unwrap() {
+            Token::Operator(OperationType::LessThanOrEqual) => {},
+            _ => panic!("wrong token")
+        }
+        match lexer.get_token().unwrap() {
+            Token::Operator(OperationType::GreaterThan) => {},
+            _ => panic!("wrong token")
+        }
+        match lexer.get_token().unwrap() {
+            Token::Operator(OperationType::GreaterThanOrEqual) => {},
+            _ => panic!("wrong token")
+        }
+        match lexer.get_token().unwrap() {
+            Token::Operator(OperationType::LessThan) => {},
+            _ => panic!("wrong token")
+        }
+        match lexer.get_token() {
+            None => {},
+            _ => panic!("wrong token")
+        }
+    }
+    #[test]
+    pub fn identifier_recognition_test() {
+        let program = String::from("?test_id ?pred-aa ?").into_bytes();
+        let lexer = LexicalAnalyzer::new(program);
+        match lexer.get_token().unwrap() {
+            Token::Identifier(x) => {
+                assert_eq!(x, &String::from("test_id"))
+            },
+            _ => panic!("wrong token")
+        }
+        match lexer.get_token().unwrap() {
+            Token::Identifier(x) => {
+                assert_eq!(x, &String::from("pred-aa"))
+            },
+            _ => panic!("wrong token")
+        }
+    }
 }
--- src/lexical_analyzer/tokenizer.rs
+++ src/lexical_analyzer/tokenizer.rs
@@ -1,4 +1,4 @@
-use std::cell::Cell;
+use std::{cell::Cell, str::{from_utf8, from_utf8_unchecked}};
 
 use super::token_types::*;
 
@@ -15,6 +15,52 @@ impl LexicalAnalyzer {
         }
     }
     pub fn get_token(&self) -> Option<Token> {
+        if let Some(char) = self.next_char() {
+            match char {
+                // Punctuations
+                p @ ('-' | ':' | '(' | ')') => {
+                    Some(Token::Punctuator(LexicalAnalyzer::punctation_type(&p)))
+                },
+                // Ordering Relations
+                p @ ('<' | '>' | '=' ) => {
+                    Some(Token::Operator(self.ordering_type(&p)))
+                },
+                // TODO: Add logical operations
+
+                // Variables
+                '?' => {
+                    let start = self.cursor.get().clone();
+                    // TODO: include line number 
+                    if start == self.program.len() {
+                        panic!("variable name cannot be empty.")
+                    }
+                    let mut next = self.program[start] as char;
+                    while !LexicalAnalyzer::is_whitespace(&next) {
+                        self.cursor.set(self.cursor.get() + 1);
+                        if self.cursor.get() == self.program.len() {
+                            break;
+                        }
+                        next = self.program[self.cursor.get()] as char;
+                        let is_dash = next != '_' || next != '-';
+                        if !next.is_alphanumeric() && !is_dash {
+                            // TODO: include line number 
+                            panic!("unrecognized variable name")
+                        }
+                    }
+                    let var_name = &self.program[start..self.cursor.get()]; 
+                    let var_name = from_utf8(var_name).unwrap_or_default();
+                    Some(Token::Identifier(var_name))
+                }
+                // Default
+                _ => None
+            }
+        } else {
+            None
+        }
+       
+    }
+
+    fn next_char(&self) -> Option<char> {
         if self.cursor.get() >= self.program.len() {
             return None
         }
@@ -24,15 +70,10 @@ impl LexicalAnalyzer {
             current = self.program[self.cursor.get()] as char;
             if self.cursor.get() == self.program.len() {
                 return None
-            }       
+            }
         }
         self.cursor.set(self.cursor.get() + 1);
-        match current {
-            p @ ('-' | ':' | '(' | ')') => {
-                Some(Token::Punctuator(LexicalAnalyzer::punctation_type(&p)))
-            },
-            _ => None
-        }
+        Some(current)
     }
 
     fn punctation_type(p: &char) -> PunctuationType {
@@ -42,6 +83,47 @@ impl LexicalAnalyzer {
             '(' => PunctuationType::LParentheses,
             ')' => PunctuationType::RParentheses,
             _   => panic!("char {} is not a punctuator.", p) 
+        }
+    }
+
+    fn ordering_type(&self, c: &char) -> OperationType {
+        match c {
+            '<' => {
+                if self.cursor.get() >= self.program.len() {
+                    return OperationType::LessThan;
+                }
+                let next = self.program[self.cursor.get()] as char;
+                match next {
+                    '=' => {
+                        self.cursor.set(self.cursor.get() + 1);
+                        OperationType::LessThanOrEqual
+                    },
+                    _ => {
+                        OperationType::LessThan
+                    }
+                }
+            }
+            '>' => {
+                if self.cursor.get() >= self.program.len() {
+                    return OperationType::GreaterThan
+                }
+                let next = self.program[self.cursor.get()] as char;
+                match next {
+                    '=' => {
+                        self.cursor.set(self.cursor.get() + 1);
+                        OperationType::GreaterThanOrEqual
+                    },
+                    _ => {
+                        OperationType::GreaterThan
+                    }
+                }
+            },
+            '=' => {
+                OperationType::Equal
+            },
+            _   => {
+                panic!("not an ordering relation");
+            }
         }
     }
 
