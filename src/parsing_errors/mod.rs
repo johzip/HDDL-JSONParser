29e871d0cf7b868963bcb3713393ff21aa1037cb
mohammad <mohammad.yousefi@anu.edu.au>
2024-09-02 10:31:47 +1000

    moved semantic errors to its corresponding module

--- src/parsing_errors/mod.rs
+++ src/parsing_errors/mod.rs
@@ -1,9 +1,11 @@
 mod lexical_errors;
 mod syntactical_errors;
 mod generic_error;
+mod semantic_errors;
 
 pub use lexical_errors::*;
 pub use syntactical_errors::*;
 pub use generic_error::*;
+pub use semantic_errors::*;
 
 use crate::lexical_analyzer::TokenType;
--- src/semantic_analyzer/mod.rs
+++ src/semantic_analyzer/mod.rs
@@ -1,10 +1,9 @@
-mod semantic_errors;
 mod analyzer;
 mod duplicate_detection;
 mod undefined_elements;
 mod tests;
 
 use crate::syntactic_analyzer::*;
-pub use semantic_errors::*;
+use crate::parsing_errors::*;
 use duplicate_detection::*;
 use undefined_elements::*;
