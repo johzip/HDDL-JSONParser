d5b2f77f5ab5420841ccbda8a8d59f786d9997e2
mohammad <mohammad.yousefi@anu.edu.au>
2024-11-20 17:04:20 +1100

    restructured integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,37 +1,30 @@
 extern crate hddl_analyzer;
 
-use std::fs;
 use hddl_analyzer::HDDLAnalyzer;
+use std::fs;
 
 #[test]
-pub fn domains_integration_test() {
-    let domain_paths = fs::read_dir("tests/domains").unwrap();
-    for path in domain_paths {
-        let domain = fs::read(path.unwrap().path());
-        if let Ok(program) = domain {
-            let result = HDDLAnalyzer::verify(&program, None);
-            if result.is_err() {
-                panic!("code has errors");
+pub fn ipc_validation_test() {
+    for folder in fs::read_dir("tests/problems/total-order").unwrap() {
+        let path = folder.as_ref().unwrap().path();
+        let domain_path = fs::read_dir(path.clone()).unwrap().filter(|x| {
+            x.as_ref().unwrap().file_name() == "domain.hddl"
+        }).next().as_ref().unwrap().as_ref().unwrap().path();
+        let domain = fs::read(&domain_path).unwrap();
+        for file in fs::read_dir(path).unwrap() {
+            if file.as_ref().unwrap().file_name() == "domain.hddl" {
+                continue;
+            } else {
+                let problem_path = file.as_ref().unwrap().path();
+                let problem = fs::read(&problem_path).unwrap();
+                match HDDLAnalyzer::verify(&domain, Some(&problem)) {
+                    Err(token) => {
+                        let error = format!("Domain: {:?} \nProblem:{:?}\nError: {:?}", domain_path, problem_path, token);
+                        panic!("{}",  error)
+                    }
+                    Ok(_) => {}
+                }
             }
-        } else {
-            panic!("error reading file");
         }
-    }
+    }    
 }
-
-// // TODO: fix
-// #[test]
-// pub fn problems_integration_test() {
-//     let problem_paths = fs::read_dir("tests/problems").unwrap();
-//     for path in problem_paths {
-//         let problem = fs::read(path.unwrap().path());
-//         if let Ok(program) = problem {
-//             let result = HDDLAnalyzer::verify(&program);
-//             if result.is_err() {
-//                 panic!("read failed");
-//             }
-//         } else {
-//             panic!("error reading file");
-//         }
-//     }
-// }
